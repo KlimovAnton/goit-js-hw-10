bbc6337f71adcb62232b9d8aead6af7e48f8fe99
KlimovAnton <109213071+KlimovAnton@users.noreply.github.com>
2024-02-19 21:14:39 +0000

    Deploying to gh-pages from @ KlimovAnton/goit-js-hw-10@e876cf6a374c0575f1b93fca1e8e5f7ce1eae4ec 🚀

---
+++ commonHelpers5.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";function s(o){console.log(`✅${o}`)}function c(o){console.log(`❌${o}`)}const r=o=>new Promise((e,n)=>{const t=Math.random()>.5;setTimeout(()=>{t?e("OKAY"):n("Error")},1e3)});r().then(s).catch(c);
+//# sourceMappingURL=commonHelpers5.js.map
